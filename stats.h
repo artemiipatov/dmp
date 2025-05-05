3c2c35d3db1c9da9cb4eca5e932d1873df5469d8
artemiipatov <patov.988@gmail.com>
2025-05-05 15:03:58 +0300

    refactor: checkpatch stats.h formatting

--- stats.h
+++ stats.h
@@ -1,40 +1,49 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
 /*
-* Read, write requests statistics
-*/
+ * Read, write requests statistics
+ */
 
 #ifndef DMP_STAT_H
 #define DMP_STAT_H
 
 struct dmp_stats {
-        unsigned long long r_ops;
-        unsigned long long w_ops;
-        unsigned long long r_size_sum;
-        unsigned long long w_size_sum;
+	unsigned long long r_ops;
+	unsigned long long w_ops;
+	unsigned long long r_size_sum;
+	unsigned long long w_size_sum;
 };
 
-static inline unsigned long long get_total_ops(struct dmp_stats *stat) {
-        return stat->r_ops + stat->w_ops;
+static inline unsigned long long get_total_ops(struct dmp_stats *stat)
+{
+	return stat->r_ops + stat->w_ops;
 }
 
-static inline unsigned long long get_total_avg_size(struct dmp_stats *stat) {
-        unsigned long long total_ops = stat->r_ops + stat->w_ops;
-        return total_ops ? (stat->r_size_sum + stat->w_size_sum) / total_ops : 0;
+static inline unsigned long long get_total_avg_size(struct dmp_stats *stat)
+{
+	unsigned long long total_ops = stat->r_ops + stat->w_ops;
+
+	return total_ops ? (stat->r_size_sum + stat->w_size_sum) / total_ops : 0;
 }
 
-static inline unsigned long long get_read_ops(struct dmp_stats *stat) {
-        return stat->r_ops;
+static inline unsigned long long get_read_ops(struct dmp_stats *stat)
+{
+	return stat->r_ops;
 }
 
-static inline unsigned long long get_read_avg_size(struct dmp_stats *stat) {
-        return stat->r_ops ? stat->r_size_sum / stat->r_ops : 0;
+static inline unsigned long long get_read_avg_size(struct dmp_stats *stat)
+{
+	return stat->r_ops ? stat->r_size_sum / stat->r_ops : 0;
 }
 
-static inline unsigned long long get_write_ops(struct dmp_stats *stat) {
-        return stat->w_ops;
+static inline unsigned long long get_write_ops(struct dmp_stats *stat)
+{
+	return stat->w_ops;
 }
 
-static inline unsigned long long get_write_avg_size(struct dmp_stats *stat) {
-        return stat->w_ops ? stat->w_size_sum / stat->w_ops : 0;
+static inline unsigned long long get_write_avg_size(struct dmp_stats *stat)
+{
+	return stat->w_ops ? stat->w_size_sum / stat->w_ops : 0;
 }
 
 #endif
--- test-dmp.sh
+++ test-dmp.sh
@@ -1,27 +1,47 @@
 #!bin/sh
 
+# Build dmp module
 make
+
+# Install dmp module
 insmod dmp.ko
 
-dd if=/dev/zero of=test_backing.img bs=4k count=1
-losetup -fP test_backing.img
+# Create loop device
+dd if=/dev/zero of=test_img.img bs=4k count=1
+losetup -fP test_img.img
 
-LOOP_DEV=$(losetup -a | grep test_backing.img | awk -F: '{print $1}')
+# Create dmp device mapper over loop device
+LOOP_DEV=$(losetup -a | grep test_img.img | awk -F: '{print $1}')
 SIZE=$(blockdev --getsz $LOOP_DEV)
 TABLE="0 $SIZE dmp $LOOP_DEV"
 dmsetup create test-dmp --table "$TABLE"
 
-TEST_STRING="HELLO LINEAR DM!"
+MAPPER_DEVICE="/dev/mapper/test-dmp"
+SYSFS_ENTRY="/sys/module/dmp/dev_stats/dm-0/reqs"
+
+# Write "Hello, World!" to dmp
+TEST_STRING="Hello, World!"
 TOTAL_BYTES=$((${#TEST_STRING} + 1))
 echo "Write:"
-echo "$TEST_STRING" | sudo dd of=/dev/mapper/test-dmp bs=1 count=$TOTAL_BYTES conv=notrunc
-cat /sys/module/dmp/dev_stats/dm-0/reqs
+echo "$TEST_STRING" | dd of=$MAPPER_DEVICE bs=1 count=$TOTAL_BYTES conv=notrunc
 
+# Read requests statistics from sysfs
+cat $SYSFS_ENTRY
+
+# Read from dmp
 echo "Read:"
-dd if=/dev/mapper/test-dmp bs=1 count=$TOTAL_BYTES status=none | cat
-cat /sys/module/dmp/dev_stats/dm-0/reqs
+dd if=$MAPPER_DEVICE bs=1 count=$TOTAL_BYTES status=none | cat
 
+# Read requests statistics from sysfs
+cat $SYSFS_ENTRY
+
+# # Remove dmp
 dmsetup remove test-dmp
+# Remove loop device
 losetup -d $LOOP_DEV
-rm test_backing.img
+rm test_img.img
+
+# Remove dmp module
 rmmod dmp
+
+exit 0
